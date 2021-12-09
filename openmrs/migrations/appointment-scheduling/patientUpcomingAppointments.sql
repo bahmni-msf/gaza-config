30fac056bf0c9c0ef1deafaac0fcb8261d334d97
tarunkumar-tw <tarunkumar.s@thoughtworks.com>
2021-12-09 12:46:14 +0530

    Tarun, Supriya | MOBN-1766 | Fix issues with multiple providers in appointment display control

--- openmrs/migrations/appointment-scheduling/patientUpcomingAppointments.sql
+++ openmrs/migrations/appointment-scheduling/patientUpcomingAppointments.sql
@@ -16,12 +16,14 @@ FROM
   JOIN appointment_service app_service
     ON app_service.appointment_service_id = pa.appointment_service_id AND app_service.voided IS FALSE
   LEFT JOIN patient_appointment_provider pap ON pap.patient_appointment_id = pa.patient_appointment_id
-  LEFT JOIN provider prov ON prov.provider_id = pap.provider_id
-  LEFT JOIN person_name psn ON psn.person_id = prov.person_id
+  LEFT JOIN provider prov ON prov.provider_id = pap.provider_id AND prov.retired IS FALSE
+  LEFT JOIN person_name psn ON psn.person_id = prov.person_id and psn.voided IS FALSE
   LEFT JOIN appointment_service_type app_service_type
     ON app_service_type.appointment_service_type_id = pa.appointment_service_type_id
 WHERE p.uuid = ${patientUuid} AND
       start_date_time >= CURDATE() AND
-      (app_service_type.voided IS FALSE OR app_service_type.voided IS NULL)
+      (app_service_type.voided IS FALSE OR app_service_type.voided IS NULL) AND
+      (pap.voided IS FALSE OR pap.voided IS NULL)
+GROUP BY pa.patient_appointment_id
 ORDER BY start_date_time ASC;"
 , 'Upcoming appointments for patient', @uuid);
